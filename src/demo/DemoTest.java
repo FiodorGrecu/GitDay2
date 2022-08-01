eb580fad876f38cbba08bab12ab0cc4d9ca6ee87
FiodorG <gre_dorelus@yahoo.com>
2022-08-01 19:17:27 -0400

    Added 2 methods one for adding and one for subtraction.

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,4 +5,15 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
+	
+	// Addition method
+	public static void add(int a , int b) {
+		System.out.println(a+b);
+	}
+	
+	// Subtraction method
+		public static void subtraction(int a , int b) {
+			System.out.println(a-b);
+		}
+	
 }

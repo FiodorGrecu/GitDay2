53fc3f37f3cc1eaca572f9c3afa5e2f8ca85a1dc
FiodorG <gre_dorelus@yahoo.com>
2022-08-03 17:50:39 -0400

    Calling the methods inside the main.

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,20 +4,23 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		add(3,4);
+		div(15,3);
 	}
-	
+
 	// Addition method
-	public static void add(int a , int b) {
-		System.out.println(a+b);
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
-	
+
 	// Subtraction method
-		public static void subtraction(int a , int b) {
-			System.out.println(a-b);
-		}
-	// Div method
-	public static void div(int a, int b){
-		System.out.println(a/b);
+	public static void subtraction(int a, int b) {
+		System.out.println(a - b);
 	}
-	
+
+	// Div method
+	public static void div(int a, int b) {
+		System.out.println(a / b);
+	}
+
 }
